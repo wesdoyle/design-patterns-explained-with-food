245c77613e2435a4f2819af326c62ac008b74ae9
Wes Doyle <wesdoyle08@gmail.com>
2021-01-17 17:37:18 -0600

    Add donut shop

---
+++ ExamplePrograms/BehavioralExamples/DonutShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DonutShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
