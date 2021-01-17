0dda5704b91d37438bfb9a0d5f06719867b6e315
Wes Doyle <wesdoyle08@gmail.com>
2021-01-17 16:20:39 -0600

    Add config tests

---
+++ Tests/ConfigurationTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using RealisticDependencies;
+using Xunit;
+
+namespace Tests {
+    public class ConfigurationTests {
+        [Fact]
+        public void Connection_String_Is_Not_Null_Or_Empty() {
+            var cxString = Configuration.ConnectionString;
+            cxString.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public void MaxConnections_Is_Greater_Than_Zero() {
+            var max = Configuration.MaxConnections;
+            max.Should().BeGreaterThan(0);
+        }
+    }
+}
